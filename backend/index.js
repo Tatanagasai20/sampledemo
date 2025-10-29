d5dec36b9ed2e18a20366600e37520ef77e3cfe6
root <root@ip-172-31-9-125.ap-south-1.compute.internal>
2025-10-29 05:06:40 +0000

    1

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const client = require('prom-client');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Prometheus metrics setup
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+});
 
 app.get('/api/todos', (req, res) => {
     res.json([
@@ -16,3 +27,4 @@ app.get('/api/todos', (req, res) => {
 app.listen(8080, () => {
     console.log('Backend server running on port 8080');
 });
+
